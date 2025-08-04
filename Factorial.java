82631a393d517b95e8bf8cb30a1b29ec2144e320
MekalaKaveri18 <153624068+MekalaKaveri18@users.noreply.github.com>
2025-08-04 22:36:18 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+class Factorial
+  public static long compute(int n){
+    if(n<0) throw new illegalArgument Exception("Negative number not allowed");
+    long res=0;
+    for(int i=2;i<=n;i++){
+      res*=i;
+    }
+    return res;
+  }
+  public static void main(String args[]){
+  int n=5;
+    System.out.println("Factorial of"+n+"is"+compute(n));
+  }
+}
